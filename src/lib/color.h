818c9cba80e313c29a517c4d899ba654a39b1ce5
Alataq <bpaquette@proton.me>
2025-10-01 18:15:37 -0400

    Making color and help command

---
+++ src/lib/color.h
@@ -0,0 +1,47 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+// Reset
+#define COLOR_RESET   "\x1b[0m"
+
+// Regular Colors
+#define COLOR_BLACK   "\x1b[30m"
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN    "\x1b[36m"
+#define COLOR_WHITE   "\x1b[37m"
+
+// Bold
+#define BOLD_BLACK   "\x1b[1;30m"
+#define BOLD_RED     "\x1b[1;31m"
+#define BOLD_GREEN   "\x1b[1;32m"
+#define BOLD_YELLOW  "\x1b[1;33m"
+#define BOLD_BLUE    "\x1b[1;34m"
+#define BOLD_MAGENTA "\x1b[1;35m"
+#define BOLD_CYAN    "\x1b[1;36m"
+#define BOLD_WHITE   "\x1b[1;37m"
+
+// Underline
+#define UNDERLINE_BLACK   "\x1b[4;30m"
+#define UNDERLINE_RED     "\x1b[4;31m"
+#define UNDERLINE_GREEN   "\x1b[4;32m"
+#define UNDERLINE_YELLOW  "\x1b[4;33m"
+#define UNDERLINE_BLUE    "\x1b[4;34m"
+#define UNDERLINE_MAGENTA "\x1b[4;35m"
+#define UNDERLINE_CYAN    "\x1b[4;36m"
+#define UNDERLINE_WHITE   "\x1b[4;37m"
+
+// Background
+#define BACKGROUND_BLACK   "\x1b[40m"
+#define BACKGROUND_RED     "\x1b[41m"
+#define BACKGROUND_GREEN   "\x1b[42m"
+#define BACKGROUND_YELLOW  "\x1b[43m"
+#define BACKGROUND_BLUE    "\x1b[44m"
+#define BACKGROUND_MAGENTA "\x1b[45m"
+#define BACKGROUND_CYAN    "\x1b[46m"
+#define BACKGROUND_WHITE   "\x1b[47m"
+
+#endif //COLOR_H
--- src/main.c
+++ src/main.c
@@ -1,13 +1,28 @@
+// Import standard libs
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]){
+// Import project's libs
+#include "lib/color.h"
+
+// Program entry point, aka main function
+int main(int argc, char *argv[]) {
     char *executable = argv[0];
-    if(argc < 2){
-        printf("No command specified, please type '%s help' to get a list of all the commands.\n", executable);
+    if(argc < 2) {
+        printf("No command specified, please type '%s help' to get a list of all commands.\n", executable);
         return 0;
     }
 
     char *command = argv[1];
-    printf("Command : %s\n", command);
+
+    if(strcmp(command, "help") == 0) {
+        printf(
+            BOLD_CYAN "Welcome to Server View Control, the cli to manage them all.\n\n" COLOR_RESET
+            BOLD_YELLOW "'help'" COLOR_RESET " - Get the list of all commands.\n"
+        );
+    } else {
+        printf(BOLD_RED "Command '%s' not found" COLOR_RESET ", please use the 'help' command to get a list of all commands.\n", command);
+    }
+
     return 0;
 }
