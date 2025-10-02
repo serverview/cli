e03d4bd38b3d2758f47b21ce16993eb111a38d09
Alataq <bpaquette@proton.me>
2025-10-02 17:21:27 -0400

    New command handler

--- src/main.c
+++ src/main.c
@@ -7,21 +7,35 @@
 
 // Program entry point, aka main function
 int main(int argc, char *argv[]) {
-    char *executable = argv[0];
-    if(argc < 2) {
-        printf("No command specified, please type '%s help' to get a list of all commands.\n", executable);
-        return 0;
+    char *command = NULL;
+    int help_flag = 0;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--help") == 0) {
+            help_flag = 1;
+        } else if (argv[i][0] != '-') {
+            if (command == NULL) {
+                command = argv[i];
+            }
+        }
     }
 
-    char *command = argv[1];
-
-    if(strcmp(command, "help") == 0) {
-        printf(
-            BOLD_CYAN "Welcome to Server View Control, the cli to manage them all.\n\n" COLOR_RESET
-            BOLD_YELLOW "'help'" COLOR_RESET " - Get the list of all commands.\n"
-        );
+    if (help_flag) {
+        if (command == NULL) {
+            printf(BOLD_CYAN "Usage: %s <command> [options]\n\n" COLOR_RESET, argv[0]);
+            printf("Commands:\n\n");
+            printf("Flags:\n");
+            printf("  " BOLD_YELLOW "--help" COLOR_RESET "    Display this help message\n");
+        } else {
+            fprintf(stderr, BOLD_RED "Help for command '%s' is not yet implemented.\n" COLOR_RESET, command);
+        }
+    } else if (command == NULL) {
+        fprintf(stderr, BOLD_RED "Usage: %s <command> [options]\n" COLOR_RESET, argv[0]);
+        fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
+        return 1;
     } else {
-        printf(BOLD_RED "Command '%s' not found" COLOR_RESET ", please use the 'help' command to get a list of all commands.\n", command);
+        fprintf(stderr, BOLD_RED "%s: '%s' is not a %s command. See '%s --help'.\n" COLOR_RESET, argv[0], command, argv[0], argv[0]);
+        return 1;
     }
 
     return 0;
