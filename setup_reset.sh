0f8a3abb81bcc9ddf51abb3645f7ba061b399357
Alataq <bpaquette@proton.me>
2025-10-02 20:51:23 -0400

    Small file to reset the setup

---
+++ setup_reset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Resetting setup..."
+
+# Remove directories
+rm -rf /var/www/svh
+rm -rf /etc/serverview
+rm -rf /var/log/serverview
+rm -rf /var/serverview
+
+echo "Setup reset complete."
